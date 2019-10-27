9bd6dd98f235d991c44962c287e23b8cfe0dc591
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-27 16:54:43 +0900

    Summay

--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'portfolio#index'
   get '/portfolio/mail', to: 'portfolio#mail'
   get '/portfolio/profile', to: 'portfolio#profile'
   get '/portfolio/work', to: 'portfolio#work'
+
 end
