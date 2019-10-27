65ed608a27142da2b7c9a7c5fe6f6346d71e5158
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-10-27 17:27:36 +0900

    Update top.js

--- portfolio/app/assets/javascripts/top.js
+++ portfolio/app/assets/javascripts/top.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-  $('.opend').on('click', function () {
-    $('.nav-bar').fadeToggle();
+  $('#opend').on('click', function () {
+    $('.nav-bar').slideToggle();
     $('.nav-bar').toggleClass('active');
   });
 });
