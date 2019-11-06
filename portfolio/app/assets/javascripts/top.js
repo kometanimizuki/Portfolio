12bb80d8fbfbeaea40f178b9c835045f3f713190
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-11-06 20:40:06 +0900

    move jquery

--- portfolio/app/assets/javascripts/top.js
+++ portfolio/app/assets/javascripts/top.js
@@ -1,6 +1,56 @@
 $(document).on('turbolinks:load', function() {
-  $('#opend').on('click', function () {
-    $('.toggle').slideToggle();
-    $('.toggle').toggleClass('active');
+  $('#title1').textillate({
+    selector: 'texts',
+    loop: false,
+    autoStart: true,
+    inEffects: [],
+    minDisplayTime: 3000,
+    initialDelay: 2000,
+
+    in: {
+      effect: 'flipInX',
+      delayScale: 1.5,
+      delay: 60,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    }
+  });
+  $('#title2').textillate({
+    selector: 'texts',
+    loop: false,
+    autoStart: true,
+    inEffects: [],
+    minDisplayTime: 3000,
+    initialDelay: 2000,
+
+    in: {
+      effect: 'flipInX',
+      delayScale: 1.5,
+      delay: 60,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    }
+  });
+  $('#title3').textillate({
+    selector: 'texts',
+    loop: false,
+    autoStart: true,
+    inEffects: [],
+    minDisplayTime: 3000,
+    initialDelay: 2000,
+
+    in: {
+      effect: 'flipInX',
+      delayScale: 1.5,
+      delay: 60,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    }
   });
 });
